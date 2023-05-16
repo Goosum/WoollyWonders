0fd272972785bd883fa5779d988c59d09996cdda
Goosums <coopercooke@yahoo.com>
2023-05-16 16:28:03 -0400

    Reverted ExtraWoollySheepFurLayer to extend SheepFurLayer Reverted ExtraWoollySheepRenderer to extend SheepRenderer

--- src/main/java/goosum/goosum/woollywonders/client/renderer/ExtraWoollySheepRenderer.java
+++ src/main/java/goosum/goosum/woollywonders/client/renderer/ExtraWoollySheepRenderer.java
@@ -6,20 +6,20 @@ import goosum.goosum.woollywonders.client.renderer.layer.ExtraWoollySheepFurLaye
 import goosum.goosum.woollywonders.common.entity.ExtraWoollySheepEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.SheepRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 
-public class ExtraWoollySheepRenderer extends MobRenderer<ExtraWoollySheepEntity, ExtraWoollySheepModel<ExtraWoollySheepEntity>> {
+public class ExtraWoollySheepRenderer extends SheepRenderer {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation(WoollyWonders.MODID, "textures/entity/extra_wooly_sheep.png");
 
-    public ExtraWoollySheepRenderer(EntityRendererProvider.Context context, ExtraWoollySheepModel<ExtraWoollySheepEntity> extraWoollySheepModel, float v) {
-        super(context, extraWoollySheepModel, v);
+    public ExtraWoollySheepRenderer(EntityRendererProvider.Context context) {
+        super(context);
         this.addLayer(new ExtraWoollySheepFurLayer(this, context.getModelSet()));
+
     }
 
-
-    @Override
     public ResourceLocation getTextureLocation(ExtraWoollySheepEntity extraWoollySheepEntity) {
         return TEXTURE;
     }
